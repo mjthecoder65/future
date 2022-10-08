522bb35dd7e34e36205db95713e25526df8c194f
Michael Jordan <mjthecoder53161@gmail.com>
2022-10-09 00:11:48 +0900

    Updated future.js

--- future.js
+++ future.js
@@ -8,27 +8,39 @@ const CONTRACT_ADDRESS = "0xea5458c2bff5c2c35a19a56c32d56abd6f5f4a55"
 
 
 function Future(contractAddress) {
-    const ABI = JSON.parse(fs.readFileSync("./contracts/abi.json"))
-    this.contract = new web3.eth.Contract(ABI, contractAddress)
+    const jsonInterface = JSON.parse(fs.readFileSync("./contracts/artifacts/abi.json"));
+    this.contractAddress = contractAddress;
+    this.contract = new web3.eth.Contract(jsonInterface, contractAddress);
 }
+
 
 Future.prototype.getContractBalance = async function () {
     const balance = await this.contract.methods.getContractBalance().call()
-    return balance;
+    return { address: this.contractAddress, balance: web3.utils.fromWei(balance, "ether"), unit: "ether"}
 }
+
 
 Future.prototype.getUserBalance = async function(address){
     const balance = await this.contract.methods.getUserBalance(address).call()
-    return balance;
+    return  { address: address, balance: web3.utils.fromWei(balance, "ether"), unit: "ether"}
+}
+
+
+Future.prototype.getUser = async function(address) {
+    const result = await this.contract.methods.getUser(address).call();
+    const [_, username, amount] = result;
+    return { address, username, amount }
 }
 
 Future.prototype.addUser = async function(address, username) {
-    // TODO
+    const result = await this.contract.methods.addUser(address, username).call()
 }
 
+
 Future.prototype.send = async function(address, amount) {
-    // TODO
+    // TODO: Typescript and Backend using JS.
 }
+
 
 Future.prototype.deposit = async function(address, amount) {
     // TODO
