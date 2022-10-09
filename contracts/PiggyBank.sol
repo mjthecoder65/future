601f813e141311b4bc36b5acf5a6dc889bcc06d3
Michael Jordan <mjthecoder53161@gmail.com>
2022-10-09 09:22:27 +0900

    renamed Future.sol and PiggyBank.sol

---
+++ contracts/PiggyBank.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.8.17;
+
+import "./Mortal.sol";
+
+contract PiggyBank is Mortal {
+
+    event LogWithdrawFundBeforeReleaseTime(address addr, uint amount, uint balance);
+    event LogFundWithdraw(address addr, uint amount, uint balance);
+    event LogFundingReceived(address addr, uint amount, uint balance);
+    event LogFundDeposited(address addr, uint amount, uint balance);
+    event LogAddedUser(address addr,string username, uint amount);
+    event LogFundTransfered(address addr, uint amount);
+
+    struct User {
+        address payable addr;
+        string username;
+        uint amount;
+    }
+
+    mapping(address => User) private users;
+
+    function addUser(address payable addr, string memory username) public onlyOwner {
+        uint amount = 0;
+        users[addr] = User(addr, username, amount);
+        emit LogAddedUser(addr, username, amount);
+    }
+
+    function getUser(address addr ) public view returns(User memory) {
+        return users[addr];
+    }
+
+    function getUserBalance(address addr) public view returns(uint){
+        return users[addr].amount;
+    }
+
+    function deposit(address addr) payable public {
+        require(users[addr].addr == addr, "Account Not Found. Failed to Deposit");
+        users[addr].amount += msg.value;
+    }
+
+    function getContractBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+
+    function canWithdraw(address addr, uint amount) private view returns(bool) {
+        if (amount < users[addr].amount) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    function send(address payable to, uint amount) public onlyOwner {
+        uint balance = getContractBalance();
+        require(balance > amount, "Insufficient Funds");
+        to.transfer(amount);
+        emit LogFundTransfered(to, amount);
+    }
+    
+    function withdraw(address payable addr, uint amount) payable public onlyOwner{
+        require(users[addr].addr == addr, "Only the account owner can withdraw");
+        require(canWithdraw(addr, amount) == true, "Insufficient funds");
+        users[addr].amount -= amount;
+        users[addr].addr.transfer(amount);
+        emit LogFundWithdraw(addr, amount, users[addr].amount);
+    } 
+}
